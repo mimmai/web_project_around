2d42db118bf2fe83f663227f8348c4b4351892da
Mitzi Manriquez <mimma.dfs@gmail.com>
2025-02-06 21:04:52 -0400

    correccion de estilos popup.css, y funcionalidad de javaScript

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 const popUp = document.querySelector(".popup");
 const editButton = document.querySelector(".profile__edit-button");
 const closeButton = popUp.querySelector(".popup__close-button");
-const formButton = document.getElementById(".form__button");
-const inputName = document.getElementById(".input-name");
+const formButton = document.querySelector(".form__button");
+const inputName = document.querySelector(".input-name");
 const inputDescription = document.getElementById(".input-description");
 const nameTitle = document.querySelector(".profile__title");
 
@@ -11,6 +11,7 @@ const nameSubtitle = document.querySelector(".profile__Subtitle");
 
 editButton.addEventListener("click", function() {
   popUp.showModal();
+  console.log(popUp)
 });
 
 closeButton.addEventListener("click", function() {
