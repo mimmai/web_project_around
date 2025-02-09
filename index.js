d8e9ae2a0bdb2c2fae681ef698234b7bebd3ce44
Mitzi Manriquez <mimma.dfs@gmail.com>
2025-02-09 19:48:05 -0400

    correcion de estilos en el titulo, y cards

--- index.js
+++ index.js
@@ -1,20 +1,19 @@
-const popUp = document.querySelector(".popup__container");
+const popup = document.querySelector(".popup__container");
 const editButton = document.querySelector(".profile__edit-button");
 const closeButton = document.querySelector(".popup__close-button");
 const formButton = document.querySelector(".form__button");
 const inputName = document.querySelector(".input-name");
 const inputDescription = document.querySelector(".input-description");
 const nameTitle = document.querySelector(".profile__title");
-
 const nameSubtitle = document.querySelector(".profile__Subtitle");
 
 
 editButton.addEventListener("click", function() {
-  popUp.showModal();
+  popup.showModal();
 });
 
 closeButton.addEventListener("click", function() {
-  popUp.close();
+popup.close();
 });
 
 
@@ -27,7 +26,7 @@ formButton.addEventListener("click", function() {
   nameTitle.textContent = nameValue
   nameSubtitle.textContent = descriptionValue
 
-  popUp.close()
+  popup.close()
 });
 
 
